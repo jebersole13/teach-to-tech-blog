c1382428cfb888db0fa5f5afc2fa5906b8fb951c
adawg2010 <jcebersole1@gmail.com>
2024-01-14 18:50:20 +0900

    toggle accomplished

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,29 @@
-
+import { useState } from "react"
+import './Navbar.css'
 function Navbar() {
+
+  const [menuOpen, setMenuOpen]=useState(false);
   return (
-    <div >
-      
-    </div>
+   <nav>
+
+     <div className="menu" onClick={()=> {
+      setMenuOpen(!menuOpen)
+     }}>
+      <span></span>
+      <span></span>
+      <span></span>
+     </div>
+    <ul className={menuOpen ? "open" : ''}>
+  <li><a href="/">Home</a></li>
+      <li><a href="#about" >About</a > </li>
+      <li><a href="#services" >Services</a ></li> 
+      <li><a href="#portfolio" >Portfolio</a ></li>
+       <li><a href="#contact" >Contact</a ></li>
+    </ul>
+   </nav>
   )
 }
+
+
 
 export default Navbar
