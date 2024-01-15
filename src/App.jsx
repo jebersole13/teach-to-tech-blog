0db1b2928de396962c11fd432eb9be5f908b9eda
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2024-01-15 14:12:06 +0900

    ToggleButton try

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import './index.css'
 import Navbar from './Components/Navbar'
+import ToggleButton from './Components/ToggleButton';
 import About from './Components/pages/about';
 import Blog from './Components/pages/blog';
 import Learning from './Components/pages/learning';
 import Podcast from './Components/pages/Podcast';
 import Footer from './Components/Footer';
-import logo_light from '..Components/assets/logo-dark.png';
-import logo_dark from '..Components/assets/logo-light.png';
-import toggle_light from '..Components/assets/night.png';
-import toggle_dark from '..Components/assets/day.png';
 import { useEffect, useState } from 'react';
 const App =()=> {
-      const toggle_mode =()=>{
-            theme == 'light' ?  setTheme('dark') : setTheme('light')
-          }
-          
-
+    
      const current_theme = localStorage.getItem( 'current_theme')
 
-      
 const [theme, setTheme] = useState( current_theme ? current_theme : 'light');
 
 useEffect(()=> {
@@ -26,12 +18,9 @@ useEffect(()=> {
 },[theme])
   return (
     <div className={`App ${theme}`}>
-          <div className='logo'>
-<a href="/">  <img src={theme =='light' ? logo_light: logo_dark} alt="lightlo" className='logo'  /></a></div>
-      
-<img onClick={()=>{toggle_mode()}} src={theme ==='light' ? toggle_light: toggle_dark} alt="" className='toggle-icon' />
- 
+        
       <Navbar theme={theme} setTheme={setTheme}/>
+      <ToggleButton/>
 
       <section id='about'>
 <About className={`App ${theme}`}/>
