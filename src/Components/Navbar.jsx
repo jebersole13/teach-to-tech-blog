6234bc210ad704e97f948bc4f9cfdbd0f8cd79e3
adawg2010 <jcebersole1@gmail.com>
2024-01-21 12:07:02 +0900

    toggling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,24 @@
 import './Navbar.css'
 import { NavLink } from "react-router-dom"
+import { motion } from "framer-motion";
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+  },
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 }
+  }
+};
 const Navbar= () =>{
 
   return (
     <div className='navbar'>
-<ul>
+<motion.ul variants={variants}>
     <li><NavLink to ='/About'>About</NavLink> </li>
       <li><NavLink to= '/'>Blog</NavLink></li>
         <li><NavLink to='/Learning' >Learning</NavLink> </li>
         <li><NavLink to='/Podcast'>Podcast</NavLink> </li>
-</ul>
+</motion.ul>
     </div>
   )
 }
