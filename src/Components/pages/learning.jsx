47ef59f4a95f53b157b613f3a01522643be33aba
adawg2010 <jcebersole1@gmail.com>
2024-02-02 18:38:43 +0900

    yabbadabbado

--- src/Components/pages/learning.jsx
+++ src/Components/pages/learning.jsx
@@ -3,7 +3,14 @@ function Learning() {
   return (
     <div >
       <h1>Lessons learned</h1>
-    </div>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, possimus dignissimos rem praesentium ut ratione id cupiditate assumenda modi? Consectetur aliquam suscipit inventore quasi amet dolores sed, tempora maiores quia.</p>
+    
+    <br />
+
+    <br />
+    <br />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, odio molestias necessitatibus fuga cumque error itaque quidem quia totam pariatur suscipit repudiandae fugit nostrum, unde dolor doloremque, voluptatibus delectus expedita!</p></div>
+    
   )
 }
 
