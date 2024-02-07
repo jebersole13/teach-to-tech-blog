e1f6982b6b405ba09ac130f1d9bd7176b2bcd1cb
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2024-02-07 13:06:45 +0900

    image add

--- src/Components/pages/blog.jsx
+++ src/Components/pages/blog.jsx
@@ -4,6 +4,7 @@
     {
       id:'0', 
       title:'From one language to another ',
+      image:'',
      date: '2024-2-7',
       content:' Teaching languages is fun. It is also exhausting. It is also, generally, underpaid.  I can say that with full confidence, as I have been struggling to make ends meet as a language teacher for the last ten year. Additionally, language teachers tend to be undervalued. '
 
@@ -18,6 +19,7 @@ const printPost = posts.map(posts =>
   <li key={posts.id}>
 <div className="postSection">
 <title>{posts.title}</title>
+<img src="{projects.image}" alt="image" />
 <h5>{posts.date} </h5>
 <p>{posts.content} </p>
 </div>
