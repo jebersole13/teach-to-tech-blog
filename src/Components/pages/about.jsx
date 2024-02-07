71b297c7c10496b40cd4506940347af0f0285c42
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2024-02-08 08:50:12 +0900

    blog post.

--- src/Components/pages/about.jsx
+++ src/Components/pages/about.jsx
@@ -4,8 +4,10 @@ function About() {
     <div >
       <h1>About</h1>
       <img src="snowpic.jpg" alt="chewbubblepeg" />
-      <p> Jon Ebersole is a teacher and web developer. As the title of the blog implies, he is still learning a lot about web development, despite working in the industry, like everyone else. Unlike everyone else, he has been a full time language educator for ten years. </p>
+      <p> Jon Ebersole is a teacher and web developer. As the title of the blog implies, he is still learning a lot about web development, despite working in the industry, like everyone else. Unlike everyone else, he has been a full time language educator for the last ten years. </p>
+      Take a look at the links below for more on him. 
 
+<link rel="stylesheet" href="https://portfolio-site-bay-chi.vercel.app/" />
       <p></p>
     </div>
   )
