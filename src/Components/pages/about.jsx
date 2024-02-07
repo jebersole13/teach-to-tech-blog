93910e89671ec31e13df2e88dd2ae27ca2bbde1a
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2024-02-08 08:53:49 +0900

    link add

--- src/Components/pages/about.jsx
+++ src/Components/pages/about.jsx
@@ -1,4 +1,4 @@
-
+import {Link} from 'react-router-dom'
 function About() {
   return (
     <div >
@@ -7,7 +7,7 @@ function About() {
       <p> Jon Ebersole is a teacher and web developer. As the title of the blog implies, he is still learning a lot about web development, despite working in the industry, like everyone else. Unlike everyone else, he has been a full time language educator for the last ten years. </p>
       Take a look at the links below for more on him. 
 
-<link rel="stylesheet" href="https://portfolio-site-bay-chi.vercel.app/" />
+<Link to='https://portfolio-site-bay-chi.vercel.app/'>JCE Productions</Link>
       <p></p>
     </div>
   )
