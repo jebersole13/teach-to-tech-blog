c9e6260eebf104d1659ba9fe99e77060daafc1e0
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2024-02-08 08:54:57 +0900

    br add

--- src/Components/pages/about.jsx
+++ src/Components/pages/about.jsx
@@ -7,6 +7,10 @@ function About() {
       <p> Jon Ebersole is a teacher and web developer. As the title of the blog implies, he is still learning a lot about web development, despite working in the industry, like everyone else. Unlike everyone else, he has been a full time language educator for the last ten years. </p>
       Take a look at the links below for more on him. 
 
+      <br />
+      <br />
+      <br />
+
 <Link to='https://portfolio-site-bay-chi.vercel.app/'>JCE Productions</Link>
       <p></p>
     </div>
