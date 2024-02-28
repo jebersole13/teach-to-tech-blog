fc8dd8424422f77f0c3f7e35374a8306a122b0f0
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2024-02-29 08:19:40 +0900

    two blogs

--- src/Components/pages/learning.jsx
+++ src/Components/pages/learning.jsx
@@ -2,6 +2,7 @@
 function Learning() {
   return (
     <div >
+       <div>
       <h1 className='headline'>Lessons learned</h1>
       <h3>PHP isn't that bad.</h3>
       <img src="php.jpg" alt="phpimg" />
@@ -31,7 +32,39 @@ function Learning() {
     
     <br />
 
-   
+    </div>
+
+  <div>
+      <h1 className='headline'>Lessons learned</h1>
+      <h3>PHP isn't that bad.</h3>
+      <img src="php.jpg" alt="phpimg" />
+      <p  >  When I was first learning web development, I tried a few false starts that left me frustrated and with nothing to show for my time. <br />
+      After months of chasing my tail, I sat down and started with HTML and worked my way through your standard HTML to CSS to JavaScript sort of course. <br />
+      What I was not aware of until I started doing paid classes was this little thing called PHP.  <br />
+      PHP for awhile seemed like an odd relic, something no one used anymore, and most people seemed to be badmouthing, like it was something from a bygone era. <br />
+      Yet, in the oddest of places I would see it come up. Then one day I was on Facebook and realized someting I had never seen before. <br />
+      Parts of Facebook are written in PHP. <br />
+      This thing some many people derided was used on one of the most popular websites on the internet.  <br />
+      Then it happened. The thing that sent me down a path to actually learning this supposedly outdated, bad programming language, that will supposedly waste your time and make you poor. <br />
+      A friend of mine had asked for my help building his website. I mentioned several options, none of which he liked, and then showed me that he had already created a basic Wordpress site.  
+      <br />
+      Low and behold, Wordpress is poweredby that oh so dreaded language. 
+
+      <br />
+
+      And it is basically essential to doing any development.
+
+      <br />
+
+      After going back and forth trying to 
+
+
+
+      </p>
+    
+    <br />
+
+    </div>
     
     </div>
     
