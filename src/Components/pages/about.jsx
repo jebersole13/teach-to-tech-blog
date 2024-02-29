2cd2c875bf09207f3be8e566f72d9480f9464073
jebersole13 <105177588+jebersole13@users.noreply.github.com>
2024-02-29 13:06:33 +0900

    links

--- src/Components/pages/about.jsx
+++ src/Components/pages/about.jsx
@@ -1,18 +1,21 @@
 import {Link} from 'react-router-dom'
+import { ExternalLink } from 'lucide-react';
 function About() {
   return (
     <div >
       <h1 className='headline'>About</h1>
       <img src="snowpic.jpg" alt="chewbubblepeg" />
-      <p> Jon Ebersole is a teacher and web developer. As the title of the blog implies, he is still learning a lot about web development, despite working in the industry, like everyone else. Unlike everyone else, he has been a full time language educator for the last ten years. </p>
-      Take a look at the links below for more on him. 
+      <p> Jon Ebersole is a web developer and English teahcer. As the title of the blog implies, he is still learning a lot about web development, despite working in the industry, like everyone else. Unlike everyone else, he has been a full time language educator for the last ten years. </p>
+     <p>Take a look at the links below for more on him. </p> 
 
       <br />
       <br />
       <br />
-      <i data-lucide="external-link"><Link to='https://portfolio-site-bay-chi.vercel.app/'>JCE Productions</Link></i>
 
-      <p></p>
+   
+      <Link to='https://portfolio-site-bay-chi.vercel.app/'>   <ExternalLink />JCE Productions</Link>
+
+      
     </div>
   )
 }
